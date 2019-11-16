bea2582e7ef1ed122d669818bc0d1d88eb4ff9dc
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-11-16 05:23:10 +0000

    oxygen: bring up for official support

--- revengeos_oxygen.mk
+++ revengeos_oxygen.mk
@@ -2,20 +2,37 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Revengeos stuff
+$(call inherit-product, vendor/revengeos/config/common_full_phone.mk)
 
 # Inherit from oxygen device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+$(call inherit-product, vendor/revengeos/config/gsm.mk)
+
+# Inherit some common AOSP stuff.
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+USE_GCAM := true
+
+# Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Build Type
+REVENGEOS_BUILDTYPE := OFFICIAL
+
 PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := oxygen
 PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_NAME := lineage_oxygen
+PRODUCT_NAME := revengeos_oxygen
 PRODUCT_MODEL := Mi Max 2
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := oxygen
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="oxygen-user 7.1.1 NMF26F 8.11.8 release-keys"
 
